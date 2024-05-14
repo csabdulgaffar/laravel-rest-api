fb65af5d1bb1d59607410a8b6ab544fbac093080
abdul <csbadulgaffar@gmail.com>
2024-05-14 20:22:46 +0530

    'first'

---
+++ app/Http/Controllers/Controller.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+
+use App\Models\Task;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    public function index()
+    {
+        $tasks = Task::all();
+
+        return response()->json($tasks);
+    }
+
+    public function store(Request $request)
+    {
+        $task = Task::create($request->all());
+
+        return response()->json($task, 201);
+    }
+
+    public function show(Task $task)
+    {
+        return response()->json($task);
+    }
+
+    public function update(Request $request, Task $task)
+    {
+        $task->update($request->all());
+
+        return response()->json($task);
+    }
+
+    public function destroy(Task $task)
+    {
+        $task->delete();
+
+        return response()->json(null, 204);
+    }
+}
+
+class Controller extends BaseController
+{
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+}
